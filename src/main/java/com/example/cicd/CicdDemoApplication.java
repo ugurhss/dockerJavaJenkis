d93b7f9ed058b6689a5396734972d1c9a0779b0a
ugurhss <ugurcandgn46@icloud.com>
2026-04-16 12:57:50 +0300

    update

--- src/main/java/com/example/cicd/CicdDemoApplication.java
+++ src/main/java/com/example/cicd/CicdDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CicdDemoApplication {
-//sdad bu bir deneme committi
+//sdad bu bir deneme committisadassasd
     public static void main(String[] args) {
         SpringApplication.run(CicdDemoApplication.class, args);
     }
